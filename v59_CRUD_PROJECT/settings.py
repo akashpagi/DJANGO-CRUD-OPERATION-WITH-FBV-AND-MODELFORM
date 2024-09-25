94dbc4c7f7c65010b92495289db6bdd751f9d832
akashpagi <88099663+akashpagi@users.noreply.github.com>
2024-09-25 13:36:32 +0530

    PythonAnywhere deployment setup

--- v59_CRUD_PROJECT/settings.py
+++ v59_CRUD_PROJECT/settings.py
@@ -9,34 +9,31 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os 
+import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR/'.env'))
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vl&=4xeoyj9@9++ybs=bg9n!xot$v0x@7bv)_0-4+0fj1-wq-3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
-
-# vercel deployment host setup
-ALLOWED_HOSTS = [
-    '.vercel.app',
-    '127.0.0.1',
-    'localhost',
-]
-# ALLOWED_HOSTS = ['django-crud-operations.vercel.app']
-
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS += os.environ.get('ALLOWED_HOSTS').split()
 
 # Application definition
 
@@ -84,16 +81,29 @@ WSGI_APPLICATION = 'v59_CRUD_PROJECT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "mydb",
+#         "USER": "akash",
+#         "PASSWORD": "akash",
+#         'HOST': 'akash.mysql.pythonanywhere-services.com',
+#         "PORT": "3306",
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "mydb",
-        "USER": "akash",
-        "PASSWORD": "akash",
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR/'db.sqlite3')
+    )
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='mysql://akash:your_mysql_password@akash.mysql.pythonanywhere-services.com/akash$mydb'
+#     )
+# }
 
 
 
@@ -131,7 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
